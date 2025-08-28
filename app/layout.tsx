33a799342205196c225fcbaab1f9dc179597be32
Vinod Jangid <infovinodjangid@gmail.com>
2025-08-28 19:21:07 +0530

    chore: update default theme to light mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,7 +65,7 @@ export default function RootLayout({
       >
         <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
